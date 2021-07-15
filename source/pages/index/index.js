8a3e5aaeec062a5b4acff7d72f5d46bce3037d99
iwebchester <iwebchester@gmail.com>
2021-07-15 12:54:29 +0500

    empty project

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -3,8 +3,3 @@ import './index.scss';
 
 //common 
 import '../../style/common.scss';
-
-//header 
-import '../../blocks/desktop/header/header';
-import '../../blocks/desktop/header/header.scss';
-import '../../blocks/desktop/wrapper/wrapper.scss';
